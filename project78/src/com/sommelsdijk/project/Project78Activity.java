de25bb76374ecc0115409759a1c05fdf823d59e0
edwin.schriek@gmail.com <edwin.schriek@gmail.com@dfa82d4e-3183-640b-30a1-ff7a169df35c>
2012-03-20 11:31:44 +0000

    Project aangemaakt

---
+++ project78/src/com/sommelsdijk/project/Project78Activity.java
@@ -0,0 +1,13 @@
+package com.sommelsdijk.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Project78Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
